a5cb60bc3d3a1ae66e4ffc145d6aa4fecfd24867
Saloni Joshi <saloni408joshi@gmail.com>
2026-06-11 21:03:54 +0530

    Create basic Express server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
-const app = require("./app");
+const express = require("express");
 
-app.listen(5000, () => {
-    console.log("Server Running");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("HRMS Backend Running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
