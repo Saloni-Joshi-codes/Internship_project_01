68ef320766fb9fad480c5ae690a5783b254cf96e
Saloni Joshi <saloni408joshi@gmail.com>
2026-06-24 20:55:46 +0530

    Add HR role based authorization for employee deletion

--- server/employeeRoutes.js
+++ server/employeeRoutes.js
@@ -12,7 +12,7 @@ const {
     getEmployeesBySalary
 } = require("./employeeController");
 
-const { verifyToken } = require("./middleware/authMiddleware");
+const { verifyToken, authorizeRole } = require("./middleware/authMiddleware");
 
 router.get("/", verifyToken, getAllEmployees);
 router.get("/search", verifyToken, getEmployeesByDepartment);
@@ -20,6 +20,11 @@ router.get("/salary", verifyToken, getEmployeesBySalary);
 router.get("/:id", verifyToken, getEmployeeById);
 router.post("/", verifyToken, createEmployee);
 router.put("/:id", verifyToken, updateEmployee);
-router.delete("/:id", verifyToken, deleteEmployee);
+router.delete(
+  "/:id",
+  verifyToken,
+  authorizeRole("HR"),
+  deleteEmployee
+);
 
 module.exports = router;
