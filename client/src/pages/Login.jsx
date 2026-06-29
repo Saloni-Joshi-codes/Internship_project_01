20e0d9b1dccf3940e5f491888f062f293a8c608a
Saloni Joshi <saloni408joshi@gmail.com>
2026-06-29 20:28:10 +0530

    Designed login page UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,41 @@
 function Login() {
   return (
-    <div>
+    <div style={{ textAlign: "center", marginTop: "80px" }}>
       <h1>Employee Payroll System</h1>
-      <h2>Login Page</h2>
+
+      <div
+        style={{
+          width: "300px",
+          margin: "30px auto",
+          padding: "20px",
+          border: "1px solid #ccc",
+          borderRadius: "10px"
+        }}
+      >
+        <h2>Login</h2>
+
+        <input
+          type="text"
+          placeholder="Username"
+          style={{ width: "100%", padding: "10px", marginBottom: "15px" }}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          style={{ width: "100%", padding: "10px", marginBottom: "15px" }}
+        />
+
+        <button
+          style={{
+            width: "100%",
+            padding: "10px",
+            cursor: "pointer"
+          }}
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 }
